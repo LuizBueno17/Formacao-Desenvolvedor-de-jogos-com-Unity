a891621ee71959913295fbc9d5ebf6470c09ea86
Luiz Bueno <luizgrbue@hotmail.com>
2019-02-16 14:38:12 -0300

    Update ControleDoMixer.cs
    Adicionado um método que seta a barra do slider conforme o volume salvo nas playersprefs

--- Assets/Scripts/Uteis/ControleDoMixer.cs
+++ Assets/Scripts/Uteis/ControleDoMixer.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class ControleDoMixer : MonoBehaviour
 {
@@ -9,18 +11,36 @@ public class ControleDoMixer : MonoBehaviour
     private AudioMixer mixer;
     [SerializeField]
     private string parametroDoMixer;
-
+    private Slider[] sliders;
+    private void Awake()
+    {
+        sliders = FindObjectsOfType<Slider>();
+    }
     private void Start()
     {
         if (PlayerPrefs.HasKey(this.parametroDoMixer))
         {
-            this.mixer.SetFloat(this.parametroDoMixer, PlayerPrefs.GetFloat(this.parametroDoMixer));
+            var volume = PlayerPrefs.GetFloat(this.parametroDoMixer);
+            this.mixer.SetFloat(this.parametroDoMixer, volume);
+            SetSliderValue(volume, this.parametroDoMixer);
         }
         else
         {
             this.mixer.SetFloat(this.parametroDoMixer, 0);
         }
     }
+
+    private void SetSliderValue(float volume, string parametroDoMixer)
+    {
+        foreach (var slider in sliders)
+        {
+            if(slider.tag == parametroDoMixer){
+                slider.value = volume;
+                return;
+            }
+        }
+    }
+
     public void MudarVolume(float volume)
     {
         this.mixer.SetFloat(this.parametroDoMixer, volume);
