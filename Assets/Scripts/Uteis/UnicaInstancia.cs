b33ab8bf835fc7d2ce26aa0ca8b8a7fa60b02f51
Luiz Bueno <luizgrbue@hotmail.com>
2019-02-16 15:19:01 -0300

    Criação da Unica instancia
    Para não repetir objetos na cena

---
+++ Assets/Scripts/Uteis/UnicaInstancia.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnicaInstancia : MonoBehaviour
+{
+    [SerializeField]
+    private bool sobrescreverExiste;
+    private void Start()
+    {
+        var outros = GameObject.FindGameObjectsWithTag(this.tag);
+
+        for (var i = 0; i < outros.Length; i++)
+        {
+            var outro = outros[i];
+
+            if (outro != this.gameObject)
+            {
+                if (this.sobrescreverExiste)
+                {
+                    GameObject.Destroy(outro);
+                }
+                else
+                {
+                    GameObject.Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
