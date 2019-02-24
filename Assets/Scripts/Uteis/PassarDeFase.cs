12cc07415d8e0f53745283349cfc31fbcf94a656
Luiz Bueno <luizgrbue@hotmail.com>
2019-02-24 18:14:17 -0300

    Passando de fase

---
+++ Assets/Scripts/Uteis/PassarDeFase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassarDeFase : MonoBehaviour
+{
+    private GameObject jogador;
+    [SerializeField]
+    private string cena;
+    void Start()
+    {
+        jogador = GameObject.FindWithTag("Jogador");
+    }
+    private void OnTriggerEnter(Collider objetoDeColisao)
+    {
+        if (objetoDeColisao.tag == "Jogador")
+        {
+            SceneManager.LoadScene(cena);
+        }
+    }
+}
