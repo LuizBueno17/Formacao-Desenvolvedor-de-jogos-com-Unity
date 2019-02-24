f295c646a315c013d4b945e6d267c6620335660b
Luiz Bueno <luizgrbue@hotmail.com>
2019-02-24 15:55:44 -0300

    Create Arma.cs
    Script para gerenciar as características de cada arma individualmente

---
+++ Assets/Scripts/Gameplay/Arma.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour
+{
+    public ReservaExtensivel reservaDeBalas;
+    public GameObject CanoDaArma;
+    public AudioClip SomDoTiro;
+    [SerializeField]
+    private float tempoEntreTiros;
+    private float tempoUltimoTiro;
+    void Update()
+    {
+        tempoUltimoTiro += Time.deltaTime;
+        var toquesNaTela = Input.touches;
+        foreach (var toque in toquesNaTela)
+        {
+            if (toque.phase == TouchPhase.Began && tempoUltimoTiro >= tempoEntreTiros)
+            {
+                this.Atirar();
+                ControlaAudio.instancia.PlayOneShot(SomDoTiro);
+                tempoUltimoTiro = 0;
+            }
+        }
+    }
+    private void Atirar()
+    {
+        if (this.reservaDeBalas.TemObjeto())
+        {
+            var bala = this.reservaDeBalas.PegarObjeto();
+            bala.transform.position = CanoDaArma.transform.position;
+            bala.transform.rotation = CanoDaArma.transform.rotation;
+        }
+    }
+}
