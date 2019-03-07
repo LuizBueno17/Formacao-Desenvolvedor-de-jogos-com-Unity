1f97e0ff162d6e1a34eceee402eb0d2cedb0ecc3
Luiz Bueno <luizgrbue@hotmail.com>
2019-03-07 08:18:09 -0300

    update pontuação

---
+++ Assets/Scripts/UI/Pontuação.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuação : MonoBehaviour
+{
+    private int quantidadeDeZumbisMortos;
+    public Text TextoQuantidadeDeZumbisMortos;
+    public void AtualizaQuantidadeDePontos(int pontos)
+    {
+        quantidadeDeZumbisMortos += pontos;
+        TextoQuantidadeDeZumbisMortos.text = string.Format("x {0}", quantidadeDeZumbisMortos);
+    }
+}
